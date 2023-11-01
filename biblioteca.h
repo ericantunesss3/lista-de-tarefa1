bd8d9027fed9fcd0f2f0f4895bb6a9a8981bcfd1
ericantunesss3 <146135253+ericantunesss3@users.noreply.github.com>
2023-11-01 18:27:36 +0000

    copiando parte 1 do projeto

---
+++ biblioteca.h
@@ -0,0 +1,27 @@
+//
+// Created by unifjtakeda on 15/09/2023.
+//
+
+#ifndef UNTITLED3_BIBLIOTECA_H
+#define UNTITLED3_BIBLIOTECA_H
+
+typedef struct  { // struct que mostra onde vai ser salvo cada tipo de conteudo
+    char descricao[300];
+    char categoria[100];
+    int prioridade;
+}Tarefa;
+
+typedef struct{ // struct que armazenará cada tarefa
+    Tarefa tarefas[100];
+    int qtd;
+}ListaDeTarefas;
+
+int criarTarefa(ListaDeTarefas *lt);
+int DeletarTarefa(ListaDeTarefas *lt);
+int ListarTarefa(ListaDeTarefas lt);
+
+void printMenu();
+int salvarLista(ListaDeTarefas *lt, char nome[]);
+int carregarLista(ListaDeTarefas *lt, char nome[]);
+
+#endif //UNTITLED3_BIBLIOTECA_H
